65a4bc0a4f59db72204b5ec1cd8dd0da9a527cb5
Agnee Mohanty <168866707+AgneeMohanty@users.noreply.github.com>
2024-08-12 14:12:02 +0530

    Add files via upload

--- containsDuplicateHshSet.py
+++ containsDuplicateHshSet.py
@@ -1,7 +1,8 @@
 def containsDuplicate(lst)->bool:
-    hashset = set()# in python we can directly create hashset with set,
+    hashset = set()# in python we can directly create hashset with set, hashset is same as python set, here we use set because it only takes unique elements as input
     for i in lst:
         if i in hashset:
             return True#if i in hashset it will ignore rest of function and return true as result, but only if its false till the last iteration, it will proceed to last part of code where it will return false.
         hashset.add(i) #This is the part which runs if  "if" is unsuccessful,we add the element to the hashset
+        print(hashset)
     return False
